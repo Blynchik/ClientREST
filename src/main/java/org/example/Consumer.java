045e4a105586c3d5d80673a21462a2cdc1e54aa3
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2022-12-06 09:58:47 +0300

    client refactored

--- src/main/java/org/example/Consumer.java
+++ src/main/java/org/example/Consumer.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -9,54 +10,65 @@ import java.util.Map;
 
 public class Consumer {
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplate();
-        Consumer consumer = new Consumer();
+        final String sensorName = "Sensor RESTClient";
 
-        consumer.sendNewSensor(restTemplate);
-        for(int i = 0; i < 1000; i ++){
-            consumer.sendNewMeasurement(restTemplate);
+        sendNewSensor(sensorName);
+
+        for (int i = 0; i < 1000; i++) {
+            sendNewMeasurement(sensorName);
         }
-        ArrayList response = consumer.getMeasurements(restTemplate);
+        ArrayList response = getMeasurements();
 
-        for(Object measurement:response){
+        for (Object measurement : response) {
             System.out.println(measurement.toString());
         }
     }
 
-    void sendNewSensor(RestTemplate restTemplate) {
-        Map<String, String> sensorToSend = new HashMap<String, String>();
-        sensorToSend.put("name", "RESTClient Sensor");
+    public static void sendNewSensor(String sensorName) {
+        final String url = "http://localhost:8080/sensors/registration";
 
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(sensorToSend);
+        Map<String, Object> sensorToSend = new HashMap<>();
+        sensorToSend.put("name", sensorName);
 
-        String newSensor = "http://localhost:8080/sensors/registration";
-        String newSensorResponse = restTemplate.postForObject(newSensor, request, String.class);
-        System.out.println(newSensorResponse);
+        makeRequest(url, sensorToSend);
     }
 
-    void sendNewMeasurement(RestTemplate restTemplate) {
-        Map<String, Object> measurementToSend = new HashMap<String, Object>();
+    public static void sendNewMeasurement(String sensorName) {
+        final String url = "http://localhost:8080/measurements/add";
+
+        Map<String, Object> measurementToSend = new HashMap<>();
         measurementToSend.put("value", getTemp());
         measurementToSend.put("raining", getRaining());
-        measurementToSend.put("sensor", Map.of("name", "RESTClient Sensor"));
+        measurementToSend.put("sensor", Map.of("name", sensorName));
 
-        HttpEntity<Map<String,Object>> request = new HttpEntity<>(measurementToSend);
-        String newMeasurement = "http://localhost:8080/measurements/add";
-        String newMeasurementResponse = restTemplate.postForObject(newMeasurement, request, String.class);
-        System.out.println(newMeasurementResponse);
+        makeRequest(url, measurementToSend);
     }
 
-    ArrayList getMeasurements(RestTemplate restTemplate){
+    public static ArrayList getMeasurements() {
         String url = "http://localhost:8080/measurements";
+        final RestTemplate restTemplate = new RestTemplate();
+
         return restTemplate.getForObject(url, ArrayList.class);
     }
 
-    private Boolean getRaining() {
-        int a = (int) (Math.random()*2);
+    private static Boolean getRaining() {
+        int a = (int) (Math.random() * 2);
         return a == 1;
     }
 
-    private Double getTemp() {
+    private static Double getTemp() {
         return Math.random() * (200) - 100;
+    }
+
+    private static void makeRequest(String url, Map<String, Object> json) {
+        final RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(json);
+
+        try {
+            String newSensorResponse = restTemplate.postForObject(url, request, String.class);
+            System.out.println("Успешно отправлено");
+        } catch (HttpClientErrorException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
     }
 }
